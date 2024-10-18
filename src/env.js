d7e75f061137632fbb68c2c49c8c63ce1dac0c35
TheCukitoDev <gabriel.diaz.aguilera@gmail.com>
2024-10-18 20:21:43 +0200

    Update workflows

--- src/env.js
+++ src/env.js
@@ -1,31 +1,31 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
 
 export const env = createEnv({
- 
-  server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-    SENTRY_AUTH_TOKEN: z.string(),
-    CLERK_SECRET_KEY: z.string(),
-  },
+	server: {
+		NODE_ENV: z.enum(['development', 'test', 'production']),
+		SENTRY_AUTH_TOKEN: z.string(),
+		CLERK_SECRET_KEY: z.string(),
+	},
 
-  
-  client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
-    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
-  },
+	client: {
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+		NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+		NEXT_PUBLIC_POSTHOG_HOST: z.string(),
+		NEXT_PUBLIC_APP_BASE_URL: z.string(),
+	},
 
-  
-  runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  },
-  
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  emptyStringAsUndefined: true,
-});
+	runtimeEnv: {
+		NODE_ENV: process.env.NODE_ENV,
+		SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+		NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+		NEXT_PUBLIC_APP_BASE_URL: process.env.NEXT_PUBLIC_APP_BASE_URL,
+	},
+
+	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+	emptyStringAsUndefined: true,
+})
